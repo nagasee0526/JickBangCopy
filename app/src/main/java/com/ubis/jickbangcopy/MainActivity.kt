686bf5de5b6b739b647badc4f09e7d210a92ba54
nagasee0526 <roronalove@gmail.com>
2020-05-31 12:12:27 +0900

    메인 액티비티가 베이스액티비티를 상속받도록한다

--- app/src/main/java/com/ubis/jickbangcopy/MainActivity.kt
+++ app/src/main/java/com/ubis/jickbangcopy/MainActivity.kt
@@ -2,12 +2,25 @@ package com.ubis.jickbangcopy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 
-class MainActivity : AppCompatActivity() {
+abstract class MainActivity() : BaseActivity(), Parcelable {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setValues()
+        setupEvents()
+    }
+
+    override fun setValues() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setupEvents() {
+        TODO("Not yet implemented")
     }
 
 
